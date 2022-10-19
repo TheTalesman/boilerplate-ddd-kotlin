32443ff92ebcbd3bd1481046c837760ccc1bd5f5
TheTalesman <alisons.vargas@mercadolivre.com>
2022-10-19 14:52:24 -0300

    chore: setup

---
+++ src/main/kotlin/com/br/dddbootstrap/Bootstrap/BootstrapApplication.kt
@@ -0,0 +1,11 @@
+package com.br.dddbootstrap.Bootstrap
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BootstrapApplication
+
+fun main(args: Array<String>) {
+	runApplication<BootstrapApplication>(*args)
+}
